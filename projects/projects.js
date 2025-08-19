8bad75b41dcbfe9ae5da805bebe9f210c5d78ee2
Shankar Mahto <shankarmahot619@gmail.com>
2025-08-19 22:37:34 +0530

    made resume changes

--- projects/projects.js
+++ projects/projects.js
@@ -13,108 +13,109 @@ document.addEventListener("DOMContentLoaded", (event) => {
     let mm = gsap.matchMedia();
 
     // nevigation when clicked on logo image
-    homeImg.addEventListener('click', () =>{
+    homeImg.addEventListener('click', () => {
         window.location.href = '/';
     })
 
 
-// Animation for menu toggle (reversed start and end states)
-menuTl.fromTo(
-    ".hamburger-menu-contianer",
-    { 
-        opacity: 0, 
-        display: "none", 
-        y: 50 
-    }, 
-    { 
-        opacity: 1, 
-        display: "block", 
-        y: 0, 
-        duration: 0.2 
-    }
-  );
-  
-  menuTl.fromTo(
-    ".menu-option-container",
-    { 
-        rotation: 8 
-    },
-    { 
-        rotation: 0, 
-        duration: 0.2 
-    },"-=.2"
-  );
-  
-  menuTl.fromTo(
-    ".resume-option",
-    { 
-        y: 20, 
-        rotation: -8 },
-    { 
-        y: 0, 
-        rotation: 0, 
-        duration: 0.2 
-    },"-=.2"
-  );
+    // Animation for menu toggle (reversed start and end states)
+    menuTl.fromTo(
+        ".hamburger-menu-contianer",
+        {
+            opacity: 0,
+            display: "none",
+            y: 50
+        },
+        {
+            opacity: 1,
+            display: "block",
+            y: 0,
+            duration: 0.2
+        }
+    );
+
+    menuTl.fromTo(
+        ".menu-option-container",
+        {
+            rotation: 8
+        },
+        {
+            rotation: 0,
+            duration: 0.2
+        }, "-=.2"
+    );
+
+    menuTl.fromTo(
+        ".resume-option",
+        {
+            y: 20,
+            rotation: -8
+        },
+        {
+            y: 0,
+            rotation: 0,
+            duration: 0.2
+        }, "-=.2"
+    );
 
     // Toggling menu on click
     menu.addEventListener('click', () => {
         toggle = !toggle;
-        if (toggle ?  menuTl.reverse() : menuTl.play());
-        if (toggle ? gsap.to(dotContainer, {rotation: 0, duration : 0.3}) : gsap.to(dotContainer, {rotation: 90, duration : 0.3}));
+        if (toggle ? menuTl.reverse() : menuTl.play());
+        if (toggle ? gsap.to(dotContainer, { rotation: 0, duration: 0.3 }) : gsap.to(dotContainer, { rotation: 90, duration: 0.3 }));
     })
 
     menuItems.forEach((menuItem) => {
         menuItem.addEventListener('click', () => {
             toggle = !toggle;
             menuTl.reverse();
-            if (toggle ? gsap.to(dotContainer, {rotation: 0, duration : 0.3}) : gsap.to(dotContainer, {rotation: 90, duration : 0.3}));
+            if (toggle ? gsap.to(dotContainer, { rotation: 0, duration: 0.3 }) : gsap.to(dotContainer, { rotation: 90, duration: 0.3 }));
         });
     });
 
 
     mm.add("(min-width: 769px)", () => {
-    projectTl.to('.project-details-one', {
+        projectTl.to('.project-details-one', {
 
-        scrollTrigger: {
-            trigger: '.project-details-one',
-            pin: true, // pin the trigger element while active
-            start: 'top top', // when the top of the trigger hits the top of the viewport
-            end: '+=800', // end after scrolling 500px beyond the start
-            // markers : true,
-        }
-    });
+            scrollTrigger: {
+                trigger: '.project-details-one',
+                pin: true, // pin the trigger element while active
+                start: 'top top', // when the top of the trigger hits the top of the viewport
+                end: '+=800', // end after scrolling 500px beyond the start
+                // markers : true,
+            }
+        });
 
-    projectTl.to('.project-deatails-two', {
+        projectTl.to('.project-deatails-two', {
 
-        scrollTrigger: {
-            trigger: '.project-deatails-two',
-            pin: true, // pin the trigger element while active
-            start: '0% 0%', // when the top of the trigger hits the top of the viewport
-            end: '+=600', // end after scrolling 500px beyond the start
-            // markers : true,
-        }
-    });
-})
+            scrollTrigger: {
+                trigger: '.project-deatails-two',
+                pin: true, // pin the trigger element while active
+                start: '0% 0%', // when the top of the trigger hits the top of the viewport
+                end: '+=600', // end after scrolling 500px beyond the start
+                // markers : true,
+            }
+        });
+    })
 
-mm.add("(max-width: 768px)", () => {
-    projectTl.to('.project-details-one', {
+    mm.add("(max-width: 768px)", () => {
+        projectTl.to('.project-details-one', {
 
-        scrollTrigger: {
-            trigger: '.project-details-one',
-            pin: false, 
-        }
-    });
+            scrollTrigger: {
+                trigger: '.project-details-one',
+                pin: false,
+            }
+        });
 
-    projectTl.to('.project-deatails-two', {
+        projectTl.to('.project-deatails-two', {
 
-        scrollTrigger: {
-            trigger: '.project-deatails-two',
-            pin: false, 
+            scrollTrigger: {
+                trigger: '.project-deatails-two',
+                pin: false,
 
-        }
-    });
-})
+            }
+        });
+    })
 
 
 
